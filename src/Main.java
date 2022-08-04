b900168fdc7b47b99ce16cef2c34f6d6174390fc
Daemontrit <slavikk1999@yandex.ru>
2022-08-04 20:26:28 +0300

    bonus system

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+       int startingBalance=200;
+       int replenishment=7000;
+       int bonus;
+       if(replenishment>1000){
+           bonus=replenishment/100;
+           startingBalance+=bonus+replenishment;
+           System.out.println("Итоговый баланс: "+startingBalance);
+       }else{
+           startingBalance+=replenishment;
+           System.out.println("Итоговый баланс: "+startingBalance);
+       }
+
+    }
+}
